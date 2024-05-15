5cf06c0c0aa67b14f444e5f5b52db331e57111c0
ricardokazuo <105627320+ricardokazuo@users.noreply.github.com>
2024-05-15 09:15:42 +0900

    Update Min_Max_Year_Month.sql

--- Min_Max_Year_Month.sql
+++ Min_Max_Year_Month.sql
@@ -6,7 +6,6 @@ IF OBJECT_ID('tempdb..#MyTempTableMax') IS NOT NULL
 DROP TABLE #MyTempTableMax;
 IF OBJECT_ID('tempdb..#FinalTable') IS NOT NULL
 DROP TABLE #FinalTable;
-
 IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[Min_Max_Year_Month]') AND type in (N'U'))
 DROP TABLE [dbo].[Min_Max_Year_Month]
 GO
@@ -20,49 +19,51 @@ CREATE TABLE [dbo].[Min_Max_Year_Month](
 	[Max] [float] NULL
 ) ON [PRIMARY]
 GO
-
 ;with cte as(
-select
-[Date]
-,[Ticker]
-,[Close]
-,min([Close]) OVER(PARTITION BY YEAR(Date),MONTH(Date),Ticker) AS 'Min'
+select 
+Ticker,
+YEAR([Date]) as Year,
+MONTH([Date]) as Month,
+min([Close]) as Min
 from Stocks_Float
-where YEAR(Date) > 1999
+group by Ticker, YEAR([Date]), MONTH([Date])
 ),
 cte_stocks as(
 	select Ticker, Date,[Close] from Stocks_Float
-)select DISTINCT cte.[Ticker]
-, YEAR(cte.[Date]) as Year
-, Month(cte.[Date]) as Month
-, [Min]
-, cte1.Date
+)select
+cte.Year
+,cte.Month
+,cte.[Ticker]
+,cte.[Min]
+,cte1.Date
+,row_number() OVER(PARTITION BY Year, Month, cte.Ticker, Min ORDER BY Date asc) AS [rn]
 into #MyTempTableMin
 from cte
-LEFT JOIN cte_stocks cte1 on cte.Ticker = cte1.Ticker and cte.[Min] = cte1.[Close]  and MONTH(cte.Date) = MONTH(cte1.Date)
-where YEAR(cte1.Date) > 1999
-
+INNER JOIN cte_stocks cte1 on cte.Ticker = cte1.Ticker and cte.[Min] = cte1.[Close] and cte.Year = YEAR(cte1.Date) and cte.Month = MONTH(cte1.Date)
+delete from #MyTempTableMin where rn > 1
 
 ;with cte as(
-select
-[Date]
-,[Ticker]
-,[Close]
-,max([Close]) OVER(PARTITION BY YEAR(Date),MONTH(Date),Ticker) AS 'Max'
+select 
+Ticker,
+YEAR([Date]) as Year,
+MONTH([Date]) as Month,
+max([Close]) as Max
 from Stocks_Float
-where YEAR(Date) > 1999
+group by Ticker, YEAR([Date]), MONTH([Date])
 ),
 cte_stocks as(
 	select Ticker, Date,[Close] from Stocks_Float
-)select DISTINCT cte.[Ticker]
-, YEAR(cte.[Date]) as Year
-, Month(cte.[Date]) as Month
-, [Max]
-, cte1.Date
+)select
+cte.Year
+,cte.Month
+,cte.[Ticker]
+,cte.[Max]
+,cte1.Date
+,row_number() OVER(PARTITION BY Year, Month, cte.Ticker, Max ORDER BY Date desc) AS [rn]
 into #MyTempTableMax
 from cte
-LEFT JOIN cte_stocks cte1 on cte.Ticker = cte1.Ticker and cte.[Max] = cte1.[Close]  and MONTH(cte.Date) = MONTH(cte1.Date)
-where YEAR(cte1.Date) > 1999
+INNER JOIN cte_stocks cte1 on cte.Ticker = cte1.Ticker and cte.[Max] = cte1.[Close] and cte.Year = YEAR(cte1.Date) and cte.Month = MONTH(cte1.Date)
+delete from #MyTempTableMax where rn > 1
 
 select DISTINCT MyMin.Ticker, MyMin.Year, MyMin.Month
 , MyMin.[Date] as 'Date_Min', MyMin.Min 
@@ -85,11 +86,3 @@ IF OBJECT_ID('tempdb..#MyTempTableMax') IS NOT NULL
 DROP TABLE #MyTempTableMax;
 IF OBJECT_ID('tempdb..#FinalTable') IS NOT NULL
 DROP TABLE #FinalTable;
-
-;WITH cte AS (
-  SELECT*, 
-     row_number() OVER(PARTITION BY Date_Min, Min, Date_Max, Max ORDER BY Date_Min desc) AS [rn]
-  FROM Min_Max_Year_Month
-)
-delete from cte
-where rn > 1
